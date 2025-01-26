016f31e636eeb336405948f52c82d09917988aff
praghu-b <prakashbalan555@gmail.com>
2025-01-26 12:16:24 +0530

    fix: installed new method of tailwindcss

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -8,27 +8,13 @@ const Login = () => {
     const [errorMessage, setErrorMessage] = useState('')
 
     const onSubmit = async () => {
-        // const response = await axios.post('')
         return
     }
 
     return (
         <section className='flex justify-center items-center min-h-screen bg-linear-to-r from-violet-400 to-blue-400'>
-            <div className="bg-white p-6">
-                <form action={onSubmit} className='p-6'>
-                    <div className='p-6'>
-                        <label htmlFor="email" className=''>Email</label>
-                        <input type="text" placeholder='Enter your email'/>
-                    </div>
-                    <div>
-                        <label htmlFor="email"></label>
-                        <input type="text" />
-                    </div>
-                    <div>
-                        <label htmlFor="name"></label>
-                        <input type="text" />
-                    </div>
-                </form>
+            <div className="bg-white rounded-lg shadow-xl p-6">
+                <h2 className='text-3xl text-violet-400 font-bold'>Login</h2>
             </div>
         </section>
     )
